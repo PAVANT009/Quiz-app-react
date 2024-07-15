26b55dd3e7a8ae6838d82ca02fecce675ffd0991
Pavan009 <kumarsangeetham9316@gmail.com>
2024-07-15 15:33:25 +0530

    completed the quiz project

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,8 @@
-import { useCallback, useState } from "react"
+import { act, useCallback, useRef, useState } from "react"
 
 import QUESTIONS from "../question.js"
-import QuestionTimer from "./QuestionTimer.jsx"
-import QuizIsCompleteImg from "../assets/quiz-complete.png"
+import Question from "./Question.jsx"
+import Summary from "./Summary.jsx"
 
 export default function Quiz() {
     const [userAnswers,setUserAnswers] = useState([])
@@ -18,31 +18,18 @@ export default function Quiz() {
 
     const handleSkipAnswer = useCallback(() => handleSelectAnswer(null),[handleSelectAnswer])
 
-     if (quizIsComplete) {
-        return <div id="summary">
-            <img src={QuizIsCompleteImg} alt="Quiz Is Complete Img" />
-            <h2>Quiz Completed</h2>
-        </div>
+    if (quizIsComplete) {
+        return <Summary userAnswers={userAnswers} />
     }
-    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers]
-    shuffledAnswers.sort(() => Math.random() - 0.5)
-
-  return (
-    <div id="quiz">
-    <div id="question">
-        <QuestionTimer 
-        key={activeQuestionIndex}
-        timeout={10000} onTimeout={handleSkipAnswer}/>
-        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-        <ul id="answers">
-            {shuffledAnswers.map(answer => 
-                (<li key={answer} className="answer">
-                   <button onClick={() =>handleSelectAnswer(answer)}>{answer}</button> 
-                </li>)
-            )}
-        </ul>
-    </div>
-     </div>
+    return (
+        <div id="quiz">
+            <Question 
+                key={activeQuestionIndex}
+                index={activeQuestionIndex}
+                onSelectAnswer={handleSelectAnswer}
+                onSkipAnswer={handleSkipAnswer}
+            />
+        </div>
     
   )
 }
